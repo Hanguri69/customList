95a78df7e86e79a557ebb58365a9fa356bc0490e
User <User@DESKTOP-1P90H9H>
2024-02-20 15:32:35 +0800

    3:32

--- CustomList/src/test/MyHashTable.java
+++ CustomList/src/test/MyHashTable.java
@@ -1,109 +1,77 @@
 package test;
 
-import java.util.ArrayList;
+public class MyHashTable extends CustomList<iNeedGeneric> {
+    // Sum of elements in the list
+    public int sum(CustomList<iNeedGeneric> sa) {
+        int sum = 0;
+        for (int i = 0; i < sa.size(); i++) {
+            sum += sa.get(i).getValue(); 
+        }
+        return sum;
+    }
 
+ 
+    public double average(CustomList<iNeedGeneric> sa) {
+        return 1.0 * sum(sa) / sa.size();
+    }
 
+    public int max(CustomList<iNeedGeneric> sa) {
+        int max = sa.get(0).getValue(); 
+        for (int i = 0; i < sa.size(); i++) {
+            if (max < sa.get(i).getValue()) { 
+                max = sa.get(i).getValue(); 
+            }
+        }
+        return max;
+    }
 
+    public int min(CustomList<iNeedGeneric> sa) {
+        int min = sa.get(0).getValue(); 
+        for (int i = 0; i < sa.size(); i++) {
+            if (min > sa.get(i).getValue()) { 
+                min = sa.get(i).getValue();
+            }
+        }
+        return min;
+    }
 
-	public class MyHashTable extends CustomList{
-		//лист дэхь элементүүдийн нийлбэрийг олох
-		public int sum(CustomList<Integer> sa)
-		{
-			int sum = 0;
-			for(int i = 0 ; i<sa.size();i++)
-			{
-				sum = sum+(int)sa.get(i);
-			}
-			return sum;
-		}
-		//лист дэх элементүүдийн дундажийг олох
-		public double average(CustomList<Integer> sa)
-		{
-			return 1.0*sum(sa)/sa.size();
-		}
-		//листээс хамгийн их элементийг олох
-		public int max(CustomList<Integer> sa)
-		{
-			int max = (int)sa.get(0);
-			for (int i =0; i<sa.size();i++)
-			{
-				if(max<(int)sa.get(i))
-				{
-					max = (int)sa.get(i);
-				}
-			}
-			return max;
-		}
-		//листээс хамгийн бага элементийг олох
-		public int min(CustomList<Integer> sa)
-		{
-			int min = (int)sa.get(0);
-			for (int i =0; i<sa.size();i++)
-			{
-				if(min>(int)sa.get(i))
-				{
-					min = (int)sa.get(i);
-				}
-			}
-			return min;
-		}
-		//лист дэх тэгш тоонуудыг олох 
-		public void even(CustomList<Integer> sa)
-		{
-			System.out.println(" ");
-			for (int i = 0;i<sa.size();i++)
-			{
-				if ((int)sa.get(i)%2 ==0)
-				{
-					System.out.print((int)sa.get(i)+" ");
-				}
-				
+    public void even(CustomList<iNeedGeneric> sa) {
+        System.out.println("Even numbers:");
+        for (int i = 0; i < sa.size(); i++) {
+            if (sa.get(i).getValue() % 2 == 0) { 
+                System.out.print(sa.get(i).getValue() + " ");
+            }
+        }
+        System.out.println(); 
+    }
 
-		
+    public void odd(CustomList<iNeedGeneric> sa) {
+        System.out.println("Odd numbers:");
+        for (int i = 0; i < sa.size(); i++) {
+            if (sa.get(i).getValue() % 2 != 0) { 
+                System.out.print(sa.get(i).getValue() + " "); 
+            }
+        }
+        System.out.println(); 
+    }
 
-			}
-			
-		}
-		//лист дэх сондгой элементүүдийг олох q52
-		public void odd(CustomList<Integer> sa)
-		{
-			System.out.println(" ");
-			for (int i = 0;i<sa.size();i++)
-			{
-				if ((int)sa.get(i)%2 !=0)
-				{
-					System.out.print((int)sa.get(i)+" ");
-				}
-				
+    public static void main(String[] args) {
+        MyHashTable mb = new MyHashTable();
 
-		
+        CustomList<iNeedGeneric> numbers = new CustomList<iNeedGeneric>();
+        
+        numbers.add(new iNeedGeneric(1));
+        numbers.add(new iNeedGeneric(2));
+        numbers.add(new iNeedGeneric(3));
+        numbers.add(new iNeedGeneric(4));
 
-			}
-			
-		}
-		public static void main(String [] args)
-		{
-			MyHashTable mb = new MyHashTable();
-		
-			CustomList<Integer> numbers = new CustomList<Integer>();
-			numbers.add(3);
-			numbers.add(43);
-			numbers.add(56);
-			numbers.add(5);
-			numbers.add(8);
+        
+        System.out.println(mb.min(numbers));
+        System.out.println(mb.max(numbers));
+        System.out.println(mb.average(numbers));
+        System.out.println(mb.sum(numbers));
 
-
-			numbers.print();
-			System.out.println(	mb.min(numbers));
-			System.out.println(	mb.max(numbers));
-			System.out.println(mb.average(numbers));
-			System.out.println(	mb.sum(numbers));
-			
-		mb.even(numbers);
-		mb.odd(numbers);
-
-			
-			
-		}
-
-	}
+        mb.even(numbers);
+        mb.odd(numbers);
+    }
+}
